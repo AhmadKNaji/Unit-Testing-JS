47776a3dc945b3f8d1ce6bad991de083119d512e
Ahmad Naji <ahmad.k.naji@gmail.com>
2020-10-08 01:58:13 +0300

    commented reason

--- cloneArray.test.js
+++ cloneArray.test.js
@@ -4,4 +4,7 @@ test('properly clones array', () => {
   const array = [1, 2, 3];
   expect(cloneArray(array)).toEqual(array);
   expect(cloneArray(array)).not.toBe(array);
+
+  //won't work since array same elements but different memory addresses
+  expect(cloneArray(array)).toBe(array);
 });
